ce9211d052db04b930a683dccc618b13c7e6c641
Hugo Nascimento Borges <hugo.borges@autotrac.com.br>
2017-10-03 15:57:53 -0300

    Comentado trecho de banco de dados completo!

--- app/templates/api/sistema/inicio.js
+++ app/templates/api/sistema/inicio.js
@@ -1,7 +1,16 @@
 module.exports = app => {
-    app.api.sistema.banco.sequelize.sync().done(() => {
-        app.listen(app.get("port"), () => {
-            console.log(`Fusion CRM API - porta ${app.get("port")}`);
-        });
+
+
+    //CASO O BANCO DE DADOS ESTEJA COMPLETE, DESCOMENTE O TRECHO ABAIXO E COMENTE O TRECHO POSTERIOR!
+
+    // app.api.sistema.banco.sequelize.sync().done(() => {
+    //     app.listen(app.get("port"), () => {
+    //         console.log(`AQUI COLOQUE O NOME DO SEU SISTEMA - porta ${app.get("port")}`);
+    //     });
+    // });
+
+    app.listen(app.get("port"), () => {
+        console.log(`AQUI COLOQUE O NOME DO SEU SISTEMA - porta ${app.get("port")}`);
     });
+
 };
